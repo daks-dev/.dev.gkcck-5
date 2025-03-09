2802539943e6ecde70510ecebde13f48e86e4a7f
daks-dev <daks.dev@ya.ru>
2025-03-09 14:54:22 +0300

    +

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -25,33 +25,36 @@
 
   <div class="frame xs:grid-cols-2 grid grid-cols-1 gap-8 md:grid-cols-3">
     {#each items as { slug, title, description, images }}
-      <a
-        class="group oversee:text-sky-500 relative place-self-center"
-        href={`/news/${slug}`}>
-        <Sign
-          class="top-10 left-2"
-          link
-          dark />
-        <Figure
-          custom={{
-            image: [
-              'mb-2 aspect-video w-48 lg:w-auto',
-              'group-oversee:scale-105 rounded-md bg-neutral-500/10',
-              'transition-transform duration-300'
-            ]
-          }}
-          data={{
-            ...images[0],
-            title,
-            description
-          }}>
-          <FormattedDate
-            slot="before"
-            class="mb-2 block tracking-wide text-slate-600 dark:text-slate-400"
-            date={slug}
-            parse="YY-MM-DD" />
-        </Figure>
-      </a>
+      {#if slug}
+        <a
+          class="group oversee:text-sky-500 relative place-self-center"
+          href={`/news/${slug}`}>
+          <Sign
+            class="top-10 left-2"
+            link
+            dark />
+          <Figure
+            custom={{
+              image: [
+                'mb-2 aspect-video w-48 lg:w-auto',
+                'group-oversee:scale-105 rounded-md bg-neutral-500/10',
+                'transition-transform duration-300'
+              ]
+            }}
+            data={{
+              ...images[0],
+              title,
+              description
+            }}>
+            {#snippet before()}
+              <FormattedDate
+                class="mb-2 block tracking-wide text-slate-600 dark:text-slate-400"
+                date={slug}
+                parse="YY-MM-DD" />
+            {/snippet}
+          </Figure>
+        </a>
+      {/if}
     {:else}
       <p>Новостей нет!</p>
     {/each}
